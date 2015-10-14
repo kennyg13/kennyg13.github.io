b2f76c171c2021bbe130ed734f99eddd9b0c208c
Kenneth Gonzalez <junior_895@yahoo.com>
2015-10-13 22:28:12 -0600

    Change

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ var extraCrap = function(){
 	$(".source").css("height", $(".source").height()/2);
 	$(".title").css("top", 0-$(".title").height());
 	
-	$(".Comments").css("margin-bottom", $(".miniBio img").height()));
+	$(".Comments").css("margin-bottom", $(".miniBio img").height());
 
 }
 hoverMenu();
